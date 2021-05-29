74e871c5e52d147cdf697b0138c4cf109ff816a7
Tanishq Joshi <58567550+tanishqj2005@users.noreply.github.com>
2021-05-29 17:46:11 +0530

    after long time

---
+++ precache-manifest.cc5cab8fe2fddd23f1c0533df9613e5c.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a54d6387c84eeb1fdc51bf3a59db08b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6cc2c4af0f9518761032",
+    "url": "/static/css/main.7db4efb9.chunk.css"
+  },
+  {
+    "revision": "66c9c3404b3ffe561800",
+    "url": "/static/js/2.4447e530.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.4447e530.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cc2c4af0f9518761032",
+    "url": "/static/js/main.db828e0c.chunk.js"
+  },
+  {
+    "revision": "51ff1df8d02dd786cb92",
+    "url": "/static/js/runtime-main.3ce2ee87.js"
+  },
+  {
+    "revision": "2edce0fd7b783d2066688f1de1552758",
+    "url": "/static/media/ball.2edce0fd.png"
+  },
+  {
+    "revision": "3958d698482870cef5287648a0f41a6b",
+    "url": "/static/media/bg.3958d698.jpg"
+  },
+  {
+    "revision": "35c8f039e86b1a28caef2a25757c7b5f",
+    "url": "/static/media/laptop.35c8f039.png"
+  },
+  {
+    "revision": "df7deb6f414987f014cd0d05190b7e0b",
+    "url": "/static/media/logo1.df7deb6f.png"
+  },
+  {
+    "revision": "2fa277552b620e8275a07a442ea8cd0f",
+    "url": "/static/media/logo2.2fa27755.png"
+  },
+  {
+    "revision": "9e496dbd48ca39b99ad51d4299b69244",
+    "url": "/static/media/logo3.9e496dbd.png"
+  },
+  {
+    "revision": "a2a1ebfe7d36d13e16780032f88b7dfa",
+    "url": "/static/media/tabla.a2a1ebfe.png"
+  },
+  {
+    "revision": "746d402f14585629c163ac5a65133569",
+    "url": "/static/media/tanishq.746d402f.jpg"
+  }
+]);
